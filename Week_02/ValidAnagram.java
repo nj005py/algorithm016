d9fe827ed93b46002cdcecd365249d52eb75a461
nj005py <py5555@hotmail.com>
2020-09-14 22:22:59 +0800

    242. 有效的字母异位词

---
+++ Week_02/ValidAnagram.java
@@ -0,0 +1,33 @@
+package info.nj005py.exercise.week2;
+
+import java.util.Arrays;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        char[] str1 = s.toCharArray();
+        char[] str2 = t.toCharArray();
+        Arrays.sort(str1);
+        Arrays.sort(str2);
+        return Arrays.equals(str1,str2);
+    }
+
+    public boolean isAnagram2(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        int[] counter = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counter[s.charAt(i) - 'a']++;
+            counter[t.charAt(i) - 'a']--;
+        }
+        for (int i : counter) {
+            if (i != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
