c20ba213293a51c114cd94c27804d09716a45c1b
nj005py <py5555@hotmail.com>
2020-09-29 07:06:39 +0800

    860. 柠檬水找零

---
+++ Week_04/LemonadeChange.java
@@ -0,0 +1,16 @@
+package info.nj005py.exercise.week4;
+
+public class LemonadeChange {
+    //860. 柠檬水找零
+    public boolean lemonadeChange(int[] bills) {
+        int five = 0, ten = 0;
+        for (int i : bills) {
+            if (i == 5) five++;
+            else if (i == 10) {five--; ten++;}
+            else if (ten > 0) {ten--; five--;}
+            else five -= 3;
+            if (five < 0) return false;
+        }
+        return true;
+    }
+}
